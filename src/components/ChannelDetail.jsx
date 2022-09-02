ca18d302240f6d6deeecb38f9f7ecbb5c73c4fc2
Kamran2810 <kmemon321@gmail.com>
2022-09-02 12:55:38 +0530

    Updated

---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+
+import { Videos, ChannelCard } from './index'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
+
+const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null)
+  const [videos, setVideos] = useState([])
+  
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+    .then((data) => setChannelDetail(data?.items[0]));
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then((data) =>
+      setVideos(data?.items)
+    );
+  }, [id])
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            background:
+              "linear-gradient(90deg, rgba(2,0,36,1) 3%, rgba(121,9,19,1) 37%, rgba(0,212,255,1) 81%)",
+            zIndex: 10,
+            height: "300px"
+          }}
+        />
+          <ChannelCard channelDetail={channelDetail} marginTop='-110px' />
+      </Box>
+      <Box display="flex" p='2'>
+        <Box sx={{ mr: { sm: '100px'}}} />
+          <Videos videos={videos}></Videos>
+      </Box>
+    </Box>
+  );
+}
+
+export default ChannelDetail
---
+++ src/utils/constants.js
@@ -0,0 +1,40 @@
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import HomeIcon from "@mui/icons-material/Home";
+import CodeIcon from "@mui/icons-material/Code";
+import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
+import SchoolIcon from "@mui/icons-material/School";
+import FaceRetouchingNaturalIcon from "@mui/icons-material/FaceRetouchingNatural";
+import CheckroomIcon from "@mui/icons-material/Checkroom";
+import GraphicEqIcon from "@mui/icons-material/GraphicEq";
+import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
+
+export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
+
+export const categories = [
+  { name: "New", icon: <HomeIcon /> },
+  { name: "Music", icon: <MusicNoteIcon /> },
+  { name: "Education", icon: <SchoolIcon /> },
+  { name: "Podcast", icon: <GraphicEqIcon /> },
+  { name: "Movie", icon: <OndemandVideoIcon /> },
+  { name: "Gaming", icon: <SportsEsportsIcon /> },
+  { name: "Live", icon: <LiveTvIcon /> },
+  { name: "Sport", icon: <FitnessCenterIcon /> },
+  { name: "Fashion", icon: <CheckroomIcon /> },
+  { name: "Beauty", icon: <FaceRetouchingNaturalIcon /> },
+  { name: "Comedy", icon: <TheaterComedyIcon /> },
+  { name: "Gym", icon: <FitnessCenterIcon /> },
+  { name: "Crypto", icon: <DeveloperModeIcon /> },
+];
+
+export const demoThumbnailUrl = "https://i.ytimg.com/vi/FUOGZ8d5apI/hqdefault.jpg";
+export const demoChannelUrl = "/channel/UCqyHTsG-5fjDKHPWOwu4KQw";
+export const demoVideoUrl = "/video/FUOGZ8d5apI";
+export const demoChannelTitle = "Most Amazing Klips";
+export const demoVideoTitle =
+  "Pune City 4k Cinematic Video | The IT City of Maharashtra Pune";
+export const demoProfilePicture =
+  "https://yt3.ggpht.com/GOaF1bgVp-7sD9S01Etk8WIjES8hmYOFfgh8y86RuRMSqRB9iqFKAHIBCpsDVYjqhAJ7MEbnhg=s900-c-k-c0x00ffffff-no-rj";
